422c570d2b4d678eee3cd7038854454d7ea17e55
RodrigoFdz01 <rodrigof3012@gmail.com>
2023-03-28 22:46:04 -0300

    -

--- PyMovieDb/py_movie_db.py
+++ PyMovieDb/py_movie_db.py
@@ -1,11 +1,23 @@
 from PyMovieDb import IMDB
 imdb = IMDB()
+#print(dir(imdb))
+
+# movie and year
 #res = imdb.search('Shazam', year=2019) # ok
-res = imdb.person_by_name("Christian Bale") 
-for data in res:
-    print(data)
+
+# Fetch  by actor name
+#res = imdb.person_by_name('Christian Bale')  
+actor_by_id = imdb.person_by_id("nm3822770")
+#print(res)
+#sprint(actor_by_id)
 
 
-# Fetch Movie by name
-# res2 = imdb.get_by_name("The Dark Knight")  ## esto esta ok
-#print(res2)
+# Fetch Movie by movie name
+movie_name = imdb.get_by_name("The Dark Knight")  ## esto esta ok
+print(movie_name[0][0])
+
+# Fetch Movie by popluar movies
+popular = imdb.popular_movies(genre=None) # list of 50 movies
+#print(popular)
+
+
